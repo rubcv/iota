1b5b8b7e69f55bf42f77ee8c5a2b86444fd586c7
rubcv <spamdesareth@gmail.com>
2021-09-28 22:54:13 +0200

    client implementation

--- src/client.py
+++ src/client.py
@@ -1,4 +1,9 @@
 import iota_client
 
+print("Info:")
+print(iota_client.__doc__)
+print(dir(iota_client))
+
+print("Connecting to testnet client")
 # client will connect to testnet by default
 client = iota_client.Client()print(client.get_info())
